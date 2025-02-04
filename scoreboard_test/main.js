53a0274122fe47b2db4bd063541e982f0ca21b38
platypus54 <66750619+platypus54@users.noreply.github.com>
2025-02-04 14:30:43 -0500

    Add files via upload

--- scoreboard_test/main.js
+++ scoreboard_test/main.js
@@ -10,9 +10,10 @@ catMap.lowerCategories.forEach((element, key) => {
 
 
 
+
+// start of div to hold tables
 let tableDiv = document.createElement('div')
 tableDiv.className = "scoreboard-container"
-
 
 // main upper table
 let upperTable = document.createElement('table');
@@ -27,12 +28,10 @@ uppperHeaders.forEach((element)=>{
   upperTableHeaderRow.appendChild(headerRow);
 })
 
-
 // add to table
 upperTable.appendChild(upperTableHeaderRow);
 
-
-
+// build upper categories table 
 catMap.upperCategories.forEach((element, fieldName) => {
   let tableCategoryRow = document.createElement('tr');
   
@@ -41,15 +40,47 @@ catMap.upperCategories.forEach((element, fieldName) => {
   
   let tableCategoryRowData = document.createElement('td');
   tableCategoryRowData.id = fieldName;
-  tableCategoryRowData.innerText = fieldName;
+  tableCategoryRowData.innerText = 0;
 
   tableCategoryRow.appendChild(tableCategoryRowName);
   tableCategoryRow.appendChild(tableCategoryRowData);
   upperTable.appendChild(tableCategoryRow);
 })
 
-tableDiv.appendChild(upperTable);
 
+let lowerTable = document.createElement('table');
+
+// upper header table rows
+let lowerTableHeaderRow = document.createElement('tr');
+const lowerHeaders = ["Lower","Score"];
+
+lowerHeaders.forEach((element)=>{
+  let headerRow = document.createElement('th');
+  headerRow.textContent = element;
+  lowerTableHeaderRow.appendChild(headerRow);
+})
+
+// add to table
+lowerTable.appendChild(lowerTableHeaderRow);
+
+// build upper categories table 
+catMap.lowerCategories.forEach((element, fieldName) => {
+  let tableCategoryRow = document.createElement('tr');
+  
+  let tableCategoryRowName = document.createElement('td');
+  tableCategoryRowName.innerText = fieldName;
+  
+  let tableCategoryRowData = document.createElement('td');
+  tableCategoryRowData.id = fieldName;
+  tableCategoryRowData.innerText = 0;
+
+  tableCategoryRow.appendChild(tableCategoryRowName);
+  tableCategoryRow.appendChild(tableCategoryRowData);
+  lowerTable.appendChild(tableCategoryRow);
+})
+
+tableDiv.appendChild(upperTable);
+tableDiv.appendChild(lowerTable);
 document.body.appendChild(tableDiv);
 
 
