1543f0e7f74ea0e4d4d604443f5a8f17dd12dfa6
platypus54 <66750619+platypus54@users.noreply.github.com>
2025-02-06 11:25:46 -0500

    Delete events.js

--- events.js
+++
@@ -1,9 +0,0 @@
-let scoreboard = new ScoreBoard();
-let dice = new DiceCollection();
-
-//dice.roll();
-
-//scoreboard.score.copy(dice.dice)
-//scoreboard.score.count();
-//scoreboard.calculateScores();
-
